0a4b43bd8e1fcef6554a298a61e668f7ba2e470e
shoud <shoud@hotmail.fr>
2015-03-02 14:24:53 +0100

    ControllerBAse

--- controller/CounterController.java
+++ controller/CounterController.java
@@ -12,6 +12,11 @@ public class CounterController
 	private CounterModel m_model;
 	//La vue de l'application
 	private GuiView m_view;	
+
+	//Variable de l'image courante	
+	
+
+
 	/**
 	* Constructeur de la classe CounterController
 	* @param m Le CounterModel de l'application
@@ -23,4 +28,25 @@ public class CounterController
 		m_view = v;
 		v.addListenersToView(this);
 	}
+	/**
+	* Permet de mettre à jour l'image courante
+	*/
+	public void setImageCourante()
+	{
+
+	}
+	/**
+	* Permet de modifier le nom de l'image courante
+	*/
+	public void setNameImageCourante(String nom)
+	{
+
+	}
+	/**
+	* Permet de modifier les tags de l'image courante
+	*/
+	public void setTagImageCourante(String tag)
+	{
+
+	} 
 }
